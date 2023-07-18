17675acbabd4c9bcd8a2fc7bc150c2c54d954927
SajaMatar <sm6r@example.com>
2023-07-18 13:02:36 +0300

    python ChatRoom

--- VariablesChat/Client.py
+++ VariablesChat/Client.py
@@ -1,35 +1,36 @@
 import socket
 import threading
-import sys
 import os
 
+
 def helloServer():
-    client_socket.send(name.encode('ascii'))  
+    client_socket.send(name.encode("ascii"))
+
 
 def recv():
     while True:
-        message = client_socket.recv(1027).decode('ascii')
-        if (message == ""):
+        message = client_socket.recv(1027).decode("ascii")
+        if message == "":
             print("\nServer is Down , Try again later....")
             os._exit(1)
         else:
             print(message)
-              
+
 
 def write():
     while True:
         message_to_send = input()
-        client_socket.send("{} : {}".format(name,message_to_send).encode('ascii'))
+        client_socket.send("{} : {}".format(name, message_to_send).encode("ascii"))
 
 
-host="127.0.0.1"
+host = "0.0.0.0"
 port = 55555
 
 name = input("Welcome to variables , enter your name : ")
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-    client_socket.connect((host,port))
+    client_socket.connect((host, port))
     helloServer()
     r = threading.Thread(target=recv)
     r.start()
@@ -37,6 +38,3 @@ try:
     w.start()
 except:
     print("Server is Down , try again later .... ")
-
-
-
